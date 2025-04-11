7402622593170f1de07bdf11d6b1cd69fa313f7f
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-04-11 23:03:43 +0300

    add and style the sidebar and dashboard header

---
+++ pages/dashboard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/assets/css/general.css" />
+    <link rel="stylesheet" href="/assets/css/userdasboard.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+      rel="stylesheet"
+    />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+      rel="stylesheet"
+    />
+
+    <title>FitTrack+ User Dashboard</title>
+  </head>
+  <body>
+    <div class="dashboard-container">
+      <main class="main">
+        <nav class="sidebar">
+          <ul>
+            <li class="logo-list">
+              <span class="sub-header heading-color logo">FitTrack+</span>
+            </li>
+            <li class="sidebar-btn">🏠 Dashboard</li>
+            <li class="sidebar-btn">📅 Workout Schedule</li>
+            <li class="sidebar-btn">🍽️ Diet Logs</li>
+            <li class="sidebar-btn">📈 Progress & Health</li>
+            <li class="sidebar-btn">📖 Nutrition Guide</li>
+            <li class="sidebar-btn">🎯 Profile</li>
+            <li class="sidebar-btn">🪵 Logout</li>
+          </ul>
+        </nav>
+        <!-- <div class="side"></div> -->
+        <div class="dashboard-main">
+          <div class="header container">
+            <h2 class="welcome-text heading-tertiary heading-color">
+              Welcome back, Abel
+            </h2>
+            <p class="header-date normal-text">Today is April 10, 2025</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  </body>
+</html>
