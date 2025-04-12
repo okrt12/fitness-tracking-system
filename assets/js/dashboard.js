6d1b8cb60f4c4a463b6bcab93cbb4dbefcc75467
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-04-12 17:46:36 +0300

    make the chart dynamic

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -8,3 +8,13 @@ const strDate = dashboardDate.toDateString().split(" ").splice(1).join(" ");
 
 const dashboardDateHtml = `<p class="header-date normal-text">Today is ${strDate}</p>`;
 dashboardHeader.insertAdjacentHTML("beforeend", dashboardDateHtml);
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// Graphs    ////////////////////////////////////////////
+// % completed
+const percent = 75;
+const circle = document.querySelector("svg circle:nth-of-type(2)");
+const pieText = document.querySelector(".pie-text");
+pieText.textContent = percent + "%";
+
+circle.setAttribute("stroke-dashoffset", 314 - (314 * percent) / 100);
