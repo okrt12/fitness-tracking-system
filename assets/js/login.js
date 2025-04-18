3597d0d41340a16c8601a23febc5ae3ba5c9db59
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-04-18 17:16:47 +0300

    fix the show hide password bug in the login page

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,7 +4,4 @@ const loginPasswordInput = document.getElementById("password");
 const loginShowIcon = document.querySelector(".login_show");
 const loginHideIcon = document.querySelector(".login_hide");
 
-console.log(loginHideIcon);
-console.log(loginShowIcon);
-
-//toggleShowHidePass(loginPasswordInput, loginHideIcon, loginShowIcon);
+toggleShowHidePass(loginPasswordInput, loginHideIcon, loginShowIcon);
