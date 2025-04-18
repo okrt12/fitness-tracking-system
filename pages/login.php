526f1e98db1a4dec31f759dc9c79291628eb4bed
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-04-18 14:52:30 +0300

    fix the style of the login page

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -21,50 +21,56 @@
     <link rel="stylesheet" href="../assets/css/login.css" />
     <link rel="stylesheet" href="../assets/css/general.css" />
 
-
     <title>FitTrack+ | Login</title>
-</head>
-<body>
-    
-<body>
-    <main class="login-section">
-        <div class="container">
-            <div class="login-container">
-                <div class="login-card cards">
-                    <div class="login-header">
-                        <h2 class="heading-secondary">Welcome Back to <span class="heading-color">FitTrack+</span></h2>
-                        <p class="cards-description">Log in to continue your fitness journey and track your progress.</p>
-                    </div>
-                    
-                    <form class="login-form" id="loginForm">
-                        <div class="form-group cards">
-                            <label for="email" class="cards-description">Email Address</label>
-                            <input type="email" id="email" class="input" placeholder="Enter your email" required>
-                        </div>
-                        
-                        <div class="form-group cards">
-                            <label for="password" class="cards-description">Password</label>
-                            <input type="password" id="password" class="input" placeholder="Enter your password" required>
-                            <div id="attempts" class="attempts-message">Attempts remaining: 2</div>
-                        </div>                        
-                        <button type="submit" class="btn login-btn">Log In</button>
-                        
-                        <div class="signup-link">
-                            <p class="cards-description">Don't have an account? <a href="#" class="text-link">Sign up</a></p>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  </head>
+
+  <body>
+    <main class="main-login">
+      <div class="login-container container">
+        <div class="login-header">
+          <h2 class="heading-secondary">
+            Welcome Back to <span class="heading-color">FitTrack+</span>
+          </h2>
         </div>
+
+        <form class="login-form cards" id="loginForm">
+          <div class="detail-value flex-col">
+            <label for="email" class="normal-text cards-description"
+              >Email Address</label
+            >
+            <input
+              type="email"
+              id="email"
+              class="input normal-text cards-description"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+
+          <div class="detail-value flex-col">
+            <label for="password" class="normal-text cards-description"
+              >Password</label
+            >
+            <input
+              type="password"
+              id="password"
+              class="input normal-text cards-description"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <button type="submit" class="btn-primary login-btn">Log In</button>
+
+          <div class="signup-link">
+            <p class="normal-text cards-description login-link">
+              Already have an account?
+              <a href="/pages/signup.php" class="text-link">Sign Ip</a>
+            </p>
+          </div>
+        </form>
+      </div>
     </main>
 
-    <script
-      type="module"
-      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
-    ></script>
-    <script
-      nomodule
-      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
-    ></script>
-</body>
+    <!-- JS -->
+  </body>
 </html>
--- pages/signup.php
+++ pages/signup.php
@@ -211,13 +211,6 @@
       </div>
     </main>
 
-    <script
-      type="module"
-      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
-    ></script>
-    <script
-      nomodule
-      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
-    ></script>
+    <!-- JS -->
   </body>
 </html>
