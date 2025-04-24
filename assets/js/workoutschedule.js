6f63a0f9ae4d35c90a7a126301cf67cf2299cb28
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-04-24 15:15:06 +0300

    make the post schedule functional

--- assets/js/workoutschedule.js
+++ assets/js/workoutschedule.js
@@ -1,36 +1,6 @@
 import { postData, workoutTypes, addOptions } from "./common.js";
 
-/*
-scheduleForm.addEventListener("submit", async function (e) {
-  e.preventDefault();
-
-  const scheduleData = {
-    workout_id: selectedID,
-    schedule_date: scheduleDateInput.value,
-    schedule_time: scheduleTimeInput.value,
-    schedule_duration: scheduleDurationInput.value,
-    weekly_repeat: isChecked,
-  };
-
-  try {
-    const response = await postData(
-      scheduleData,
-      "../backend/controllers/postWorkoutSchedule.php"
-    );
-    console.log(response);
-    if (response.ok) {
-      const data = await response.json();
-      alert(data.message);
-    }
-  } catch (error) {
-    console.log("Error: ", error);
-    alert("An unexpected error occurred. Please try again later.");
-  }
-});
-*/
-
 const scheduleForm = document.querySelector(".schedule-workout__form");
-const scheduleDateInput = document.getElementById("schedule-date");
 const scheduleTypeInput = document.getElementById("schedule-type");
 const scheduleDayInput = document.getElementById("schedule-day");
 const scheduleTimeInput = document.getElementById("schedule-time");
@@ -51,15 +21,6 @@ let selectedID;
     selectedID = scheduleTypeInput.value;
   });
 })();
-
-let isChecked;
-let selectedName;
-scheduleRepeatInput.addEventListener("change", () => {
-  isChecked = scheduleRepeatInput.checked;
-  selectedName =
-    scheduleRepeatInput.options[scheduleRepeatInput.selectedIndex].text;
-  console.log(selectedName);
-});
 
 scheduleForm.addEventListener("submit", async function (e) {
   e.preventDefault();
